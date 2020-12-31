21d5195eda50afd2247dbfcf41ad635613db0592
houwenchao.mouse <houwenchao.mouse@bytedance.com>
2020-12-31 12:31:04 +0800

    list
    Change-Id: I0be0efd58a15a2ede18a905d7cec54c78b530ea2

--- list.py
+++ list.py
@@ -39,3 +39,41 @@ color.reverse()
 print(color)
 
 print(len(color))
+
+# 遍历
+# Python根据缩进来判断代码行与前一个代码行的关系
+for co in color:
+    print(co)
+print("end")
+
+numbers = list(range(1,5))
+print(numbers)
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+for value in range(1,5):
+    print(value)
+
+squares = [value**2 for value in range(1, 11)]
+print(squares)
+
+# 切片
+print(squares[0:3])
+print(squares[:3])
+print(squares[3:])
+
+# 复制列表而互不影响
+# other_squares = squares会互相影响
+other_squares = squares[:]
+
+# 不能修改的值称为不可变的，而不可变的列表被称为元组
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+for dimension in dimensions:
+    print(dimension)
+# 报错
+dimensions[0] = 300
+print(dimensions[0])
+
+
